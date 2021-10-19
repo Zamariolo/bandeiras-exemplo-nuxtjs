960f1d51446bf2f026f047e254614b4d09275a3c
Zamariolo <30295966+Zamariolo@users.noreply.github.com>
2021-10-19 09:33:48 -0300

    Testes populando autocompletes corretamente

--- test/unit/BarraPesquisa.spec.js
+++ test/unit/BarraPesquisa.spec.js
@@ -27,10 +27,7 @@ beforeEach(() => {
 
   // Montando a vuex store
   state = {
-    searchOptions: () => {
-      let retorno = { filtrarPor: "", filtro: "" };
-      return retorno;
-    }
+    searchOptions: { filtrarPor: "", filtro: "" }
   };
 
   actions = {
@@ -65,13 +62,42 @@ describe("Inicializando a barra de pesquisa", () => {
     await wrapper.setData({ tipoFiltroSelecionado: "" });
     expect(filtro.isVisible()).toBe(false);
   }),
-    test("Verifica se o primeiro autocomplete possua os dados para popular", async () => {
+    test("Verifica se o primeiro autocomplete possui os dados para popular", async () => {
       //axios.get.mockResolvedValue({ data: createdMockResponse });
 
       const wrapper = shallowMount(BarraPesquisa, { store, localVue });
       const autocomplete_tipoFiltro = wrapper.find("#autocomplete_tipoFiltro");
 
       expect(autocomplete_tipoFiltro.vm.items).toEqual(wrapper.vm.tipoFiltros);
+    }),
+    test("Popula os autocompletes com a ultima pesquisa, se ja houve pesquisas", async () => {
+      // Configurando ultimas pesquisas
+      let valorTipoFiltro = "Língua";
+      let valorFiltro = "French";
+
+      store.state.searchOptions.filtrarPor = valorTipoFiltro;
+      store.state.searchOptions.filtro = valorFiltro;
+
+      const wrapper = shallowMount(BarraPesquisa, { store, localVue });
+
+      await flushPromises();
+
+      const autocomplete_tipoFiltro = wrapper.find("#autocomplete_tipoFiltro");
+      const autocomplete_filtro = wrapper.find("#autocomplete_filtro");
+
+      expect(autocomplete_tipoFiltro.vm.value).toMatch(valorTipoFiltro);
+      expect(autocomplete_filtro.vm.value).toMatch(valorFiltro);
+    }),
+    test("Autocomplete 1 vazio e autocomplete 2 escondido se nao houve ultima pesquisa", async () => {
+      const wrapper = shallowMount(BarraPesquisa, { store, localVue });
+
+      await flushPromises();
+
+      const autocomplete_tipoFiltro = wrapper.find("#autocomplete_tipoFiltro");
+      const autocomplete_filtro = wrapper.find("#autocomplete_filtro");
+
+      expect(autocomplete_tipoFiltro.vm.value).toBeFalsy()
+      expect(autocomplete_filtro.isVisible()).toBeFalsy()
     });
 });
 
@@ -102,7 +128,7 @@ describe("Populando autocomplete filtro corretamente", () => {
     expect(valoresExibidos).toEqual(valoresEsperados); // Verifica opcoes de regiao
   });
 
-  test.only("Carregando capitais corretamente", async () => {
+  test("Carregando capitais corretamente", async () => {
     const tipoFiltro = "Capital";
 
     const wrapper = shallowMount(BarraPesquisa, {
@@ -128,5 +154,75 @@ describe("Populando autocomplete filtro corretamente", () => {
     expect(autocomplete_tipoFiltro.vm.value).toMatch(tipoFiltro); // Verifica v-model tipoFiltroSelecionado foi alterado
     expect(autocomplete_filtro.isVisible()).toBe(true); //Verifica que esta visivel
     expect(valoresExibidos).toEqual(valoresEsperados); // Verifica opcoes de regiao
-  });
+  }),
+    test("Carregando Língua corretamente", async () => {
+      const tipoFiltro = "Língua";
+
+      const wrapper = shallowMount(BarraPesquisa, {
+        store,
+        localVue,
+        data() {
+          return { tipoFiltroSelecionado: tipoFiltro };
+        }
+      });
+
+      await flushPromises();
+      const autocomplete_tipoFiltro = wrapper.find("#autocomplete_tipoFiltro");
+      const autocomplete_filtro = wrapper.find("#autocomplete_filtro");
+
+      const valoresEsperados = wrapper.vm.todosIdiomas
+        .map(idioma => idioma.name)
+        .sort();
+      const valoresExibidos = autocomplete_filtro.vm.items;
+
+      expect(autocomplete_tipoFiltro.vm.value).toMatch(tipoFiltro);
+      expect(autocomplete_filtro.isVisible()).toBe(true);
+      expect(valoresExibidos).toEqual(valoresEsperados);
+    }),
+    test("Carregando Países corretamente", async () => {
+      const tipoFiltro = "País";
+
+      const wrapper = shallowMount(BarraPesquisa, {
+        store,
+        localVue,
+        data() {
+          return { tipoFiltroSelecionado: tipoFiltro };
+        }
+      });
+
+      await flushPromises();
+      const autocomplete_tipoFiltro = wrapper.find("#autocomplete_tipoFiltro");
+      const autocomplete_filtro = wrapper.find("#autocomplete_filtro");
+
+      const valoresEsperados = wrapper.vm.todosPaises
+        .map(pais => pais.name)
+        .sort();
+      const valoresExibidos = autocomplete_filtro.vm.items;
+
+      expect(autocomplete_tipoFiltro.vm.value).toMatch(tipoFiltro);
+      expect(autocomplete_filtro.isVisible()).toBe(true);
+      expect(valoresExibidos).toEqual(valoresEsperados);
+    }),
+    test("Carregando Códigos de Ligação corretamente", async () => {
+      const tipoFiltro = "Código de ligação";
+
+      const wrapper = shallowMount(BarraPesquisa, {
+        store,
+        localVue,
+        data() {
+          return { tipoFiltroSelecionado: tipoFiltro };
+        }
+      });
+
+      await flushPromises();
+      const autocomplete_tipoFiltro = wrapper.find("#autocomplete_tipoFiltro");
+      const autocomplete_filtro = wrapper.find("#autocomplete_filtro");
+
+      const valoresEsperados = wrapper.vm.todosCodigosLigacao;
+      const valoresExibidos = autocomplete_filtro.vm.items;
+
+      expect(autocomplete_tipoFiltro.vm.value).toMatch(tipoFiltro);
+      expect(autocomplete_filtro.isVisible()).toBe(true);
+      expect(valoresExibidos).toEqual(valoresEsperados);
+    });
 });
